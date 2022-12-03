89caca8a0c5ace65f180d893f0b880bf9d2ce57e
Kfir Yehezkel <kfir.ye@gmail.com>
2022-12-03 16:59:28 +0200

    addedncodeql parsing result.

--- source/RepoAnalyzerCodeql.py
+++ source/RepoAnalyzerCodeql.py
@@ -12,18 +12,41 @@ class RepoAnalyzerCodeql(RepoFilter):
 
     def execute_impl(self):
         lang = 'Java'
-        self.dbfile = f'../data/codeqldb/{self.name}/{lang}'
+        self.dbfolder = f'../data/codeqldb/{self.name}/{lang}'
 
         self.resfolder = f'../data/codeql/{self.name}'
         os.makedirs(self.resfolder, exist_ok=True)
 
-        # C:\GIT\JceFinalProject\repoScanner\data\codeql\1.SARIF C:\Users\kfiry\.codeql\packages\codeql\java-queries\0.4.4\Metrics
-        if not os.path.exists(self.dbfile):
+        if not os.path.exists(self.dbfolder):
+            print(f'No {lang} codeql db for {self.fullname}, skipping' )
             return
 
+        folders = ['codeql_db', 'java']
+        db_analysisfolder = f'{self.dbfolder}/codeql_db'
+        for x in folders:
+            db_analysisfolder = f'{self.dbfolder}/{x}'
+            if os.path.exists(f'{db_analysisfolder}/codeql-database.yml'):
+                break
+
+        # C:\GIT\JceFinalProject\repoScanner\data\codeql\1.SARIF C:\Users\kfiry\.codeql\packages\codeql\java-queries\0.4.4\Metrics
         s = os.sep
-        cmd = f'..{s}bin{s}codeql{s}codeql database analyze {self.dbfile} --format=sarif-latest --output {self.resfolder}/{lang}.SARIF ..{s}bin{s}codeql{s}packages{s}java-queries{s}0.4.4{s}Metrics'
+        cmd = f'..{s}bin{s}codeql{s}codeql database analyze {db_analysisfolder} --format=sarif-latest --output {self.resfolder}/{lang}.SARIF ..{s}bin{s}codeql{s}packages{s}java-queries{s}0.4.4{s}Metrics'
+        print(f'executing.. {cmd}')
         os.system(cmd)
+        self.output_result(db_analysisfolder)
+
+    def output_result(self, db_analysisfolder):
+        s = os.sep
+        ext = '.bqrs'
+        for root, _, files in os.walk(db_analysisfolder):
+            for filename in files:
+                file_path = os.path.join(root, filename)
+                if file_path[-(len(ext)):] == ext:
+                    cmd = f'..{s}bin{s}codeql{s}codeql bqrs decode --output={self.resfolder}/{filename[:-len(ext)]}.csv --format=csv {file_path}'
+                    print(f'executing.. {cmd}')
+                    os.system(cmd)
+
+
 
 # codeql database analyze C:\GIT\JceFinalProject\repoScanner\data\codeqldb\java\piggymetrics_java\codeql_db --format=sarif-latest --output C:\GIT\JceFinalProject\repoScanner\data\codeql\1.SARIF C:\Users\kfiry\.codeql\packages\codeql\java-queries\0.4.4\Metrics
 # bqrs decode --output=C:\GIT\JceFinalProject\repoScanner\data\codeql\TPercentaageOfComments.res  --format=csv C:\GIT\JceFinalProject\repoScanner\data\codeqldb\java\piggymetrics_java\codeql_db\results\codeql\java-queries\Metrics\RefTypes\TPercentageOfComments.bqrs
