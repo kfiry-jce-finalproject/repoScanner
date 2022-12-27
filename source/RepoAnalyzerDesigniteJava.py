015cefe22f02a6d3c8839a36e2084349bb02dfb3
Kfir Yehezkel <kfir.ye@gmail.com>
2022-12-28 00:54:40 +0200

    adding bin files folders, and DesigniteJava scanner methods.

---
+++ source/RepoAnalyzerDesigniteJava.py
@@ -0,0 +1,28 @@
+from RepoFilter import RepoFilter
+import os
+import platform
+import pandas as pd
+
+class RepoAnalyzerDesigniteJava(RepoFilter):
+
+    def __init__(self):
+        super().__init__()
+        self.source = 'designiteJava'
+
+    def execute_impl(self):
+        folder = f'../tmp/{self.name}'
+        s = os.sep
+        _exec = f'java  -Xmx4096m -jar .{s}..{s}bin{s}DesigniteJava{s}DesigniteJava.jar'
+        cmd = f'{_exec} -i {folder} -o ../data/designiteJava/{self.name}'
+        print(cmd)
+        os.system(cmd)
+
+    def getMetricResults(self):
+        infile = f'../data/designiteJava/{self.name}/designCodeSmells.csv'
+        # create element tree object
+        df = pd.read_csv(infile)
+        if len(df) == 0:
+            return {}
+        tmp_dic = df[['Type Name', 'Code Smell']].groupby(['Code Smell']).count()['Type Name'].to_dict()
+        totals = {key: str(value) for key, value in tmp_dic.items()}
+        return totals
